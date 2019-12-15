f5e953feb3f96ac63397e69a0027c514ddf6496a
Henry Ho <i_think_i_be@hotmail.com>
2019-12-15 13:04:45 +0800

    add more line parameters

--- src/LiveLogMonitor/Program.cs
+++ src/LiveLogMonitor/Program.cs
@@ -7,6 +7,8 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
+using CommandLine;
+
 namespace LiveLogMonitor
 {
     internal static class Program
@@ -25,27 +27,59 @@ namespace LiveLogMonitor
             public string Message { get; set; }
         }
 
+        private class Options
+        {
+            [Option('m', "max", Required = false, Default = false, HelpText = "Set screen size to max.")]
+            public bool Max { get; set; }
+            [Option('t', "top", Required = false, Default = true, HelpText = "Always on top.")]
+            public bool Top { get; set; }
+            [Value(0, MetaName = "pipename", HelpText = "Pipe name [host\\port]")]
+            public long? PipeName { get; set; }
+        }
+
         private static async Task Main(string[] args)
         {
-            string host = null, named_pipe = null;
-            if (args.Length == 0)
+            string host = ".", named_pipe = null;
+
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
             {
-                host = ".";
-                named_pipe = EnumerateLocalNamedPipes();
-            }
-            else
-            {
-                string[] strs = args[0].Split('/', '\\', ':');
-                if (strs.Length == 2)
+                if (o.PipeName.HasValue)
                 {
-                    host = strs[0];
-                    named_pipe = strs[1];
+                    string[] strs = args[0].Split('/', '\\', ':');
+                    if (strs.Length == 2)
+                    {
+                        host = strs[0];
+                        named_pipe = strs[1];
+                    }
                 }
-            }
+
+                if (o.Top)
+                {
+                    IntPtr hWnd = NativeMethods.GetConsoleWindow();
+                    if (hWnd != IntPtr.Zero)
+                        NativeMethods.SetWindowPos(hWnd, new IntPtr(NativeMethods.HWND_TOPMOST), 0, 0, 0, 0,
+                            NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE);
+                }
+
+                if (o.Max)
+                {
+                    Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                        NativeMethods.ShowWindow(Process.GetCurrentProcess().MainWindowHandle, ShowWindowMode.SW_MAXIMIZE);
+                }
+            }).WithNotParsed(errs =>
+            {
+                //Console.WriteLine(errs);
+                Console.WriteLine("without pipename, the tool will enumerate local host named pipes, connect first of which name start of log***)");
+                Environment.Exit(-1);
+            });
+
+            if(string.IsNullOrEmpty(named_pipe))
+                named_pipe = EnumerateLocalNamedPipes();
 
             if (string.IsNullOrEmpty(host) || string.IsNullOrEmpty(named_pipe))
             {
-                printUsage();
+                Console.WriteLine("not found any named pipe start with log_*");
                 Console.WriteLine("Press any key to exit.");
                 Console.ReadKey();
                 Environment.Exit(0);
@@ -53,9 +87,6 @@ namespace LiveLogMonitor
 
             Console.OutputEncoding = Encoding.Unicode;
             Console.Title = $@"Living Log Monitor 【host:{host} pipe:{named_pipe}】";
-            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                NativeMethods.ShowWindow(Process.GetCurrentProcess().MainWindowHandle, ShowWindowMode.SW_MAXIMIZE);
 
             var pipeClient = new NamedPipeClientStream(host, named_pipe, PipeDirection.InOut);
             Console.WriteLine("If you disconnect and then connect again, you need to wait for the first log");
@@ -73,7 +104,7 @@ namespace LiveLogMonitor
                     if (!pipeClient.IsConnected) throw new Exception("pipe is broken");
 
                     var log = ReadFromStream(pipeClient, buffer);
-                    if(log.ID == 0 && log.TimeStamp == DateTimeOffset.MinValue)
+                    if (log.ID == 0 && log.TimeStamp == DateTimeOffset.MinValue)
                         continue;
                     if (log.Level == LogLevel.Warning)
                         Console.ForegroundColor = ConsoleColor.Yellow;
@@ -98,16 +129,16 @@ namespace LiveLogMonitor
             }
         }
 
-        private static void printUsage()
-        {
-            Console.WriteLine("not found any named pipe start with log_*");
-            Console.WriteLine();
-            Console.WriteLine("Usage:");
-            Console.WriteLine();
-            Console.WriteLine("LiveLogMonitor [host/named_pipe]");
-            Console.WriteLine("without parameters, the tool will enumerate local host named pipes, connect first of which name start of log***)");
-            Console.WriteLine();
-        }
+        //private static void printUsage()
+        //{
+        //    Console.WriteLine("not found any named pipe start with log_*");
+        //    Console.WriteLine();
+        //    Console.WriteLine("Usage:");
+        //    Console.WriteLine();
+        //    Console.WriteLine("LiveLogMonitor [host/named_pipe] [top:[on/off]]");
+        //    Console.WriteLine("without parameters, the tool will enumerate local host named pipes, connect first of which name start of log***)");
+        //    Console.WriteLine();
+        //}
 
         private static string EnumerateLocalNamedPipes()
         {
@@ -179,6 +210,40 @@ namespace LiveLogMonitor
         /// <returns></returns>
         [DllImport("user32.dll")]
         public static extern bool ShowWindow(IntPtr hWnd, ShowWindowMode cmdShow);
+
+        /// <summary>
+        /// GetConsoleWindow
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+
+        /// <summary>
+        /// SetWindowPos
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="hWndInsertAfter"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="cx"></param>
+        /// <param name="cy"></param>
+        /// <param name="uFlags"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(
+            IntPtr hWnd,
+            IntPtr hWndInsertAfter,
+            int x,
+            int y,
+            int cx,
+            int cy,
+            int uFlags);
+
+        public const int HWND_TOPMOST = -1;
+        public const int SWP_NOMOVE = 0x0002;
+        public const int SWP_NOSIZE = 0x0001;
+
     }
 
 }
